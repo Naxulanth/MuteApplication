ea8960ecf5fe53876b1350d853f33c0e6df40b1f
Naxulanth <Naxulanth@gmail.com>
2017-08-27 05:07:31 +0300

    pp

--- src/MuteApplication.cpp
+++ src/MuteApplication.cpp
@@ -20,6 +20,7 @@ const IID IID_ISimpleAudioVolume = __uuidof(ISimpleAudioVolume);
 const IID IID_IAudioStreamVolume = __uuidof(IAudioStreamVolume);
 const IID IID_IAudioSessionControl = __uuidof(IAudioSessionControl);
 const IID IID_IAudioSessionManager2 = __uuidof(IAudioSessionManager2);
+const IID IID_IAudioSessionManager = __uuidof(IAudioSessionManager);
 
 #define REFTIMES_PER_SEC  10000000
 #define REFTIMES_PER_MILLISEC  10000
@@ -44,16 +45,19 @@ void findEndpoint() {
 	IAudioStreamVolume *pVolume = NULL;
 	REFERENCE_TIME ref = REFTIMES_PER_SEC;
 	IAudioSessionControl *pControl = NULL;
-	IAudioSessionManager2 *pManager = NULL;
+	IAudioSessionManager2 *pManager2 = NULL;
+	IAudioSessionManager *pManager = NULL;
 	IAudioSessionEnumerator *pSessions = NULL;
 	IAudioSessionControl2 *pControl2 = NULL;
 	UINT count;
 	int sessionCount = 0;
 	LPWSTR pProcessName = NULL;
+	wchar_t processName;
 	PROPVARIANT varName;
 	float pfLevel;
 	DWORD pProcessId;
-	
+	GUID guid;
+	LPCGUID pGuid = &guid; // lpcguid = pointer to constant guid
 
 	// Initialize COM library
 	hr = CoInitializeEx(NULL, COINIT_MULTITHREADED);
@@ -96,48 +100,66 @@ void findEndpoint() {
 		// Print endpoint friendly name and endpoint ID.
 		printf("Default endpoint: \"%S\" (%S)\n", varName.pwszVal, pwszID);
 
-	hr = pEndpoint->Activate(IID_IAudioSessionManager2, CLSCTX_ALL, NULL, (void**)&pManager);
+	hr = pEndpoint->Activate(IID_IAudioSessionManager2, CLSCTX_ALL, NULL, (void**)&pManager2);
 	EXIT_ON_ERROR(hr)
 
-		hr = pManager->GetSessionEnumerator(&pSessions);
+		hr = pEndpoint->Activate(IID_IAudioSessionManager, CLSCTX_ALL, NULL, (void**)&pManager);
+	EXIT_ON_ERROR(hr)
+
+		hr = pManager2->GetSessionEnumerator(&pSessions);
 	EXIT_ON_ERROR(hr)
 
 		hr = pSessions->GetCount(&sessionCount);
 	EXIT_ON_ERROR(hr)
 
+
 		std::cout << sessionCount << std::endl;
 
-	for (int index = 0; index < sessionCount; index++)
+	 for (int index = 0; index < sessionCount; index++)
 	{
+		CoTaskMemFree(pProcessName);
+		SAFE_RELEASE(pControl);
+
 		hr = pSessions->GetSession(index, &pControl);
 		EXIT_ON_ERROR(hr)
 			hr = pControl->GetDisplayName(&pProcessName);
+			processName = *pProcessName;
 		EXIT_ON_ERROR(hr)
 			hr = pControl->QueryInterface<IAudioSessionControl2>(&pControl2);
 		EXIT_ON_ERROR(hr)
+			hr = pControl2->GetGroupingParam(&guid);
+		EXIT_ON_ERROR(hr)
 			hr = pControl2->GetProcessId(&pProcessId);
 		EXIT_ON_ERROR(hr)
-			std::cout << std::to_string(pProcessId) + ":::::::::::::: " + std::to_string(index) << std::endl;
+			std::cout << std::to_string(pProcessId) + "::::::::::::::::::" + std::to_string(index) + "^^^^^" + std::to_string(processName) << std::endl;
+		hr = pManager->GetSimpleAudioVolume(pGuid, TRUE, &psVolume);
+		EXIT_ON_ERROR(hr)
+			hr = psVolume->SetMute(FALSE, NULL);
 	}
 
-	// Cleanup
-exit:
+	exit:
 	_com_error err(hr);
 	LPCTSTR errMsg = err.ErrorMessage();
 	printf(errMsg);
 	CoTaskMemFree(pwszID);
 	CoTaskMemFree(wave);
+	CoTaskMemFree(pProcessName);
 	pwszID = NULL;
+	wave = NULL;
 	PropVariantClear(&varName);
 	SAFE_RELEASE(pSessions);
-	SAFE_RELEASE(pManager);
+	SAFE_RELEASE(pManager2);
+	SAFE_RELEASE(pManager)
 	SAFE_RELEASE(pProps)
-		SAFE_RELEASE(pEndpoint)
-		SAFE_RELEASE(pEnumerator)
-		SAFE_RELEASE(pCollection)
-		SAFE_RELEASE(pClient)
-		SAFE_RELEASE(pVolume)
-		system("pause");
+	SAFE_RELEASE(pEndpoint)
+	SAFE_RELEASE(pEnumerator)
+	SAFE_RELEASE(pCollection)
+	SAFE_RELEASE(pClient)
+	SAFE_RELEASE(pVolume)
+	SAFE_RELEASE(psVolume)
+	SAFE_RELEASE(pControl)
+	SAFE_RELEASE(pControl2)
+	system("pause");
 	return;
 }
 
